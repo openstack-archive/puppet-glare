12305351286d10fdb58e3cf4983c0e6b5bac9499
Tobias Urdin <tobias@busybox.se>
2018-05-22 00:00:29 +0200

    Fix lint, syntax and structuring
    Change-Id: Id5400ac336f5a49e5c9b8748f928deb56812e075

--- manifests/backend/rbd.pp
+++ manifests/backend/rbd.pp
@@ -1,55 +1,49 @@
-# == class: glare::backend::rbd
+# == Class: glare::backend::rbd
 #
-# configures the storage backend for glare
-# as a rbd instance
+# Configures the storage backend for glare
+# as a rbd instance.
 #
-# === parameters:
+# === Parameters:
 #
-#  [*rbd_store_user*]
-#    Optional. Default: $::os_service_default.
+# [*rbd_store_user*]
+#   (Optional) Default: $::os_service_default.
 #
-#  [*rbd_store_pool*]
-#    Optional. Default: $::os_service_default.
+# [*rbd_store_pool*]
+#   (Optional) Default: $::os_service_default.
 #
-#  [*rbd_store_ceph_conf*]
-#    Optional. Default: $::os_service_default.
+# [*rbd_store_ceph_conf*]
+#   (Optional) Default: $::os_service_default.
 #
-#  [*rbd_store_chunk_size*]
-#    Optional. Default: $::os_service_default.
+# [*rbd_store_chunk_size*]
+#   (Optional) Default: $::os_service_default.
 #
-#  [*show_image_direct_url*]
-#    Optional. Enables direct COW from glare to rbd
-#    DEPRECATED, use show_image_direct_url in glare::api
+# [*package_manage*]
+#   (Optional) Whether manage ceph package state or not.
+#   Defaults to true
 #
-#  [*package_manage*]
-#      (optional) Whether manage ceph package
-#      state or not.
-#      Defaults to true.
+# [*package_ensure*]
+#   (Optional) Desired ensure state of packages.
+#   Defaults to present
 #
-#  [*package_ensure*]
-#      (optional) Desired ensure state of packages.
-#      accepts latest or specific versions.
-#      Defaults to present.
-#
-#  [*rados_connect_timeout*]
-#      Optinal. Timeout value (in seconds) used when connecting
-#      to ceph cluster. If value <= 0, no timeout is set and
-#      default librados value is used.
-#      Default: $::os_service_default.
+# [*rados_connect_timeout*]
+#   (Optional) Timeout value (in seconds) used when connecting
+#   to ceph cluster. If value <= 0, no timeout is set and
+#   default librados value is used.
+#   Default: $::os_service_default
 #
 # [*multi_store*]
-#   (optional) Boolean describing if multiple backends will be configured
+#   (Optional) Boolean describing if multiple backends will be configured.
 #   Defaults to false
 #
 class glare::backend::rbd(
-  $rbd_store_user         = $::os_service_default,
-  $rbd_store_ceph_conf    = $::os_service_default,
-  $rbd_store_pool         = $::os_service_default,
-  $rbd_store_chunk_size   = $::os_service_default,
-  $package_manage         = true,
-  $package_ensure         = 'present',
-  $rados_connect_timeout  = $::os_service_default,
-  $multi_store            = false,
+  $rbd_store_user        = $::os_service_default,
+  $rbd_store_ceph_conf   = $::os_service_default,
+  $rbd_store_pool        = $::os_service_default,
+  $rbd_store_chunk_size  = $::os_service_default,
+  $package_manage        = true,
+  $package_ensure        = 'present',
+  $rados_connect_timeout = $::os_service_default,
+  $multi_store           = false,
 ) {
 
   include ::glare::deps
@@ -69,13 +63,11 @@ class glare::backend::rbd(
   }
 
   if $package_manage {
-    ensure_packages('python-ceph',
-        {
-          ensure => $package_ensure,
-          name   => $::glare::params::pyceph_package_name,
-          tag    => 'glare-support-package',
-        }
-    )
+    ensure_packages('python-ceph', {
+      ensure => $package_ensure,
+      name   => $::glare::params::pyceph_package_name,
+      tag    => 'glare-support-package',
+    })
   }
 
 }
