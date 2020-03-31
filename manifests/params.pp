76189930cf6b667d6e1b68680934cfae29ba1878
Takashi Kajinami <tkajinam@redhat.com>
2020-04-01 08:28:20 +0900

    Drop compatibility for el6
    ... because it's now really old, and we don't expect any users
    use it to run OpenStack.
    
    Change-Id: I8db6b0166759bf535e577d10e8afaa36dcbaff86

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,17 +9,13 @@ class glare::params {
 
   case $::osfamily {
     'RedHat': {
-      $glare_package_name    = 'openstack-glare'
-      $glare_service_name    = 'openstack-glare-api'
-      if ($::operatingsystem != 'fedora' and versioncmp($::operatingsystemrelease, '7') < 0) {
-        $pyceph_package_name = "python${pyvers}-ceph"
-      } else {
-        $pyceph_package_name = "python${pyvers}-rbd"
-      }
+      $glare_package_name  = 'openstack-glare'
+      $glare_service_name  = 'openstack-glare-api'
+      $pyceph_package_name = "python${pyvers}-rbd"
     }
     'Debian': {
-      $glare_package_name    = 'glare-api'
-      $glare_service_name    = 'glare-api'
+      $glare_package_name  = 'glare-api'
+      $glare_service_name  = 'glare-api'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} operatingsystem")
